33423fb57d26a4c2c4d7d7a486d819bf920f10b2
Asier <Asier@Asier-Portatil>
2015-11-15 23:29:59 +0100

    Mejorado webStorage

--- web/js/lib/webstorage.js
+++ web/js/lib/webstorage.js
@@ -1,76 +1,87 @@
 
 //Estructura de almacenaje
-var tipoStorage=["localStorage","sessionStorage"];
-var seleccionStorage=0;
 
-function guardarEnJSON(prefijo,id,cadena){
-    eval(tipoStorage[seleccionStorage]+'.setItem("'+prefijo+'_'+id+'", JSON.parse('+cadena+'))');
-}
+BDL = new BD(0);
+BDS = new BD(1);
 
-function cargarEnJSON(prefijo, id){
-    return eval(tipoStorage[seleccionStorage]+'.getItem("'+prefijo+'_'+id+'")');
-}
+function BD(storageSelection) {
 
-function eliminarEnJSON(prefijo, id){
-    eval(tipoStorage[seleccionStorage]+'.removeItem("'+prefijo+'_'+id+'")');
-}
+    var tipoStorage = ["localStorage", "sessionStorage"];
 
-//funciones usuario
-function guardarUsuario(nick, password, email, edad, altura, peso, genero, ciudad, cp, constitucion, foto) {
-    var usuarioJSON = '{' + '"nick":"' + nick + '", "password":"' + password + '", "email":"' + email + '", "edad":"' + edad + '", altura":"' + altura + '", "peso":"' + peso + '", "genero":"' + genero + '", "ciudad":"' + ciudad + '", ' + '"cp":"' + cp + '", "constitucion":"' + constitucion + '", "foto":"' + foto + '"' + '}';
-    guardarEnJSON("usuario",nick,usuarioJSON);
-}
+    this.storage = eval(tipoStorage[storageSelection]);
 
-function cargarUsuario(nick) {
-    return cargarEnJSON("usuario",nick);
-}
+    //privados
+    this.guardarEnJSON = function (prefijo, id, cadena) {
+        this.storage.setItem(prefijo + "_" + id, JSON.parse(cadena));
+    };
 
-function eliminarUsuario(nick){
-    eliminarEnJSON("usuario",nick);
-    eliminarEnJSON("aficiones",nick);
-    eliminarEnJSON("caracteres",nick);
-}
+    this.cargarEnJSON = function (prefijo, id) {
+        return this.storage.getItem(prefijo + "_" + id);
+    };
 
-//funciones aficiones de usuario
-function guardarAficionesUsuario(nick, aficiones) {
-    var aficionesJSON = '[';
-    for (var i = 0; i < aficiones.length; i++) {
-        aficionesJSON += '{"nombre":"' + aficiones[i] + '"}';
-        if (i !== aficiones.length - 1) {
-            aficiones += ',';
+    this.eliminarEnJSON = function (prefijo, id) {
+        this.storage.removeItem(prefijo + "_" + id);
+    };
+
+    //publicos
+    //funciones usuario
+    this.guardarUsuario = function (nick, password, email, edad, altura, peso, genero, ciudad, cp, constitucion, foto) {
+        var usuarioJSON = '{' + '"nick":"' + nick + '", "password":"' + password + '", "email":"' + email + '", "edad":"' + edad + '", altura":"' + altura + '", "peso":"' + peso + '", "genero":"' + genero + '", "ciudad":"' + ciudad + '", ' + '"cp":"' + cp + '", "constitucion":"' + constitucion + '", "foto":"' + foto + '"' + '}';
+        this.guardarEnJSON("usuario", nick, usuarioJSON);
+    };
+
+    this.cargarUsuario = function (nick) {
+        return this.cargarEnJSON("usuario", nick);
+    };
+
+    this.eliminarUsuariofunction = function (nick) {
+        this.eliminarEnJSON("usuario", nick);
+        this.eliminarEnJSON("aficiones", nick);
+        this.eliminarEnJSON("caracteres", nick);
+    };
+
+    //funciones aficiones de usuario
+    this.guardarAficionesUsuario = function (nick, aficiones) {
+        var aficionesJSON = '[';
+        for (var i = 0; i < aficiones.length; i++) {
+            aficionesJSON += '{"nombre":"' + aficiones[i] + '"}';
+            if (i !== aficiones.length - 1) {
+                aficiones += ',';
+            }
         }
-    }
-    aficionesJSON += ']';
-    guardarEnJSON("aficiones",nick,aficionesJSON);
-}
+        aficionesJSON += ']';
+        this.guardarEnJSON("aficiones", nick, aficionesJSON);
+    };
 
-function cargarAficionesUsuario(nick) {
-    return cargarEnJSON("aficiones",nick);
-}
+    this.cargarAficionesUsuario = function (nick) {
+        return this.cargarEnJSON("aficiones", nick);
+    };
 
-//funciones carácteres de usuario
-function guardarCaracteresUsuario(nick, caracteres) {
-    var caracteresJSON = '[';
-    for (var i = 0; i < caracteres.length; i++) {
-        caracteresJSON += '{"nombre":"' + caracteres[i] + '"}';
-        if (i !== caracteres.length - 1) {
-            caracteres += ',';
+    //funciones carácteres de usuario
+    this.guardarCaracteresUsuario = function (nick, caracteres) {
+        var caracteresJSON = '[';
+        for (var i = 0; i < caracteres.length; i++) {
+            caracteresJSON += '{"nombre":"' + caracteres[i] + '"}';
+            if (i !== caracteres.length - 1) {
+                caracteres += ',';
+            }
         }
-    }
-    caracteresJSON += ']';
-    guardarEnJSON("caracteres",nick,caracteresJSON);
+        caracteresJSON += ']';
+        this.guardarEnJSON("caracteres", nick, caracteresJSON);
+    };
+
+    this.cargarCaracteresUsuario = function (nick) {
+        return this.cargarEnJSON("caracteres", nick);
+    };
+
+    //funciones de login
+    this.esUsuarioCorrecto = function (nick, password) {
+        var usuarioJSON = cargarEnJSON("usuario", nick);
+        if (usuarioJSON.password === password) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
 
-function cargarCaracteresUsuario(nick) {
-    return cargarEnJSON("caracteres",nick);
-}
-
-//funciones de login
-function esUsuarioCorrecto(nick, password) {
-    var usuarioJSON = cargarEnJSON("usuario",nick);
-    if(usuarioJSON.password === password){
-        return true;
-    }else{
-        return false;
-    }
-}
